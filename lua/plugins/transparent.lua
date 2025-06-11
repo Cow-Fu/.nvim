0977f5d43834f6dbff17b2700b0b70b1e71833a1
nathan <nathanj758@gmail.com>
2025-06-11 17:00:14 -0400

    add command to toggle transparency

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,3 +1,15 @@
+-- Function to toggle transparency
+local function toggle_transparency()
+  local current_transparency = require("tokyonight.config").options.transparent
+  require("tokyonight").setup({
+    transparent = not current_transparency,
+  })
+  vim.cmd("colorscheme tokyonight") -- Reload the colorscheme to apply changes
+end
+
+-- Create a command to toggle transparency
+vim.api.nvim_create_user_command("Trans", toggle_transparency, {})
+
 return {
   "tokyonight.nvim",
   opts = {
